1b0e1ddf501e1c86c10775f7790925eef7b45844
Simon111 <Simon111@c8f6423a-0e81-4fc8-99ba-8b429ac74a24>
2007-09-07 15:20:02 +0000

    This commit was generated by cvs2svn to compensate for changes in r4, which included commits to RCS files with non-trunk default branches.

--- rtl/key_computer.v
+++ rtl/key_computer.v
@@ -6,9 +6,15 @@
 // refer:
 //			cas.c in vlc opensource project
 
+// this module implement the function csa_ComputeKey 
+// and cw accord to ck
+//     key accord to kk;
+
+// 
+
 module key_computer(cw,key);
-		input 	[63:0]	cw;
-		output	[447:0]	key;
+		input 	[63:0]	cw;		// input cw, 	has 64bits
+		output	[447:0]	key;    // output key, 	has 7*64bits
 		
 		wire	[63:0]	key1;
 		wire	[63:0]	key2;
@@ -16,22 +22,21 @@ module key_computer(cw,key);
 		wire	[63:0]	key4;
 		wire	[63:0]	key5;
 		wire	[63:0]	key6;
-		wire	[63:0]	key7;
 		
-		key_perm kp0 (   cw, key7 );
-		key_perm kp1 ( key7, key6 );
-		key_perm kp2 ( key6, key5 );
-		key_perm kp3 ( key5, key4 );
-		key_perm kp4 ( key4, key3 );
-		key_perm kp5 ( key3, key2 );
-		key_perm kp6 ( key2, key1 );
 		
-		assign key[64*1-1:64*0]=key2 ^ 64'h0000000000000000;
-		assign key[64*2-1:64*1]=key3 ^ 64'h0101010101010101;
-		assign key[64*3-1:64*2]=key4 ^ 64'h0202020202020202;
-		assign key[64*4-1:64*3]=key5 ^ 64'h0303030303030303;
-		assign key[64*5-1:64*4]=key6 ^ 64'h0404040404040404;
-		assign key[64*6-1:64*5]=key7 ^ 64'h0505050505050505;
+		key_perm kp0 (   cw, key6 );
+		key_perm kp1 ( key6, key5 );
+		key_perm kp2 ( key5, key4 );
+		key_perm kp3 ( key4, key3 );
+		key_perm kp4 ( key3, key2 );
+		key_perm kp5 ( key2, key1 );
+		
+		assign key[64*1-1:64*0]=key1 ^ 64'h0000000000000000;
+		assign key[64*2-1:64*1]=key2 ^ 64'h0101010101010101;
+		assign key[64*3-1:64*2]=key3 ^ 64'h0202020202020202;
+		assign key[64*4-1:64*3]=key4 ^ 64'h0303030303030303;
+		assign key[64*5-1:64*4]=key5 ^ 64'h0404040404040404;
+		assign key[64*6-1:64*5]=key6 ^ 64'h0505050505050505;
 		assign key[64*7-1:64*6]=cw   ^ 64'h0606060606060606;
 		
 endmodule
